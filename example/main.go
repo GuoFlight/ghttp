d8801f588cbb74b788da641a04b112357cd54631
GuoFlight <1754811791@qq.com>
2022-05-14 22:59:49 +0800

    add Example

--- example/main.go
+++ example/main.go
@@ -1,1 +1,29 @@
-package example
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/GuoFlight/ghttp"
+	"github.com/avast/retry-go"
+)
+
+func main() {
+	client := ghttp.Client{
+		Method: ghttp.MethodGet,
+		Url:    "https://www.baidu.com",
+		Retry: ghttp.Retry{
+			JudgeRetryFunc: func(res *ghttp.Res) error {
+				return errors.New("error")		//模拟错误
+			},
+			Options: []retry.Option{
+				retry.Attempts(2),			//最多重试2次
+			},
+		},
+	}
+	res, err := client.Do()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(res.Detail.StatusCode)
+}
